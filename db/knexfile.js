4933de36c36d298d7c1ee9c435143dcca788c355
Cosmin Turcin <cosmin.turcin@udevoffice.ro>
2019-02-13 12:24:06 +0000

    Merged in feat/CTB-8-data-base-setup (pull request #3)
    Feat | CTB-8 | Data base setup
    
    * feat: add docker file for posgre
    
    * feat: add db details in env
    
    * feat: add knex db config
    
    * feat: add migration for create users table
    
    * chore(gitignore): Add yarn.lock file to gitignore
    
    * chore: Remove yarn.lock file from repo
    
    Approved-by: Cristi Pirvulescu <cristi.pirvulescu@udevoffice.ro>

---
+++ db/knexfile.js
@@ -0,0 +1,66 @@
+require('dotenv').config({
+  path: `../env-files/${process.env.NODE_ENV || 'development'}.env`,
+});
+
+global.Promise = require('bluebird');
+// knexfile.js
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: '../migrations',
+      tableName: 'migrations',
+    },
+    seeds: {
+      directory: '../seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: '../migrations',
+      tableName: 'migrations',
+    },
+    seeds: {
+      directory: '../seeds',
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
